8ca81324d98a7ed4869921cd3a9bf26c3b152edc
MdSultanAnwar <mdsultananwar@gmail.com>
2026-07-15 17:50:59 +0530

    init

--- src/main/java/com/ecommerce/dao/OrderDAO.java
+++ src/main/java/com/ecommerce/dao/OrderDAO.java
@@ -2,4 +2,23 @@ package com.ecommerce.dao;
 
 public class OrderDAO {
 
+    public void addCustomer() {
+
+    }
+
+    public void getCustomerById(int customerId) {
+
+    }
+
+    public void getAllCustomers() {
+
+    }
+
+    public void updateCustomer() {
+
+    }
+
+    public void deleteCustomer(int customerId) {
+
+    }
 }
