7824164a095e724d081e52455da528fda8ba8307
Skale <Skale@Kashif>
2026-07-16 11:21:37 +0530

    updats

--- src/main/java/com/ecommerce/App.java
+++ src/main/java/com/ecommerce/App.java
@@ -1,12 +1,35 @@
 package com.ecommerce;
 
- 
+import java.sql.SQLException;
+
+import com.ecommerce.dao.CustomerDAO;
+import com.ecommerce.model.Customer;
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
-        System.out.println("Durga");
-        System.err.println();
+//        Customer cust = new Customer("Shakir", "asdcdss@dsd", "98765436");
+//        CustomerDAO dao = new CustomerDAO();
+//        try
+//		{
+//			dao.registerCustomer(cust);
+//		} catch (ClassNotFoundException | SQLException e)
+//		{
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//        
+//        try
+//		{
+//			Customer customer= dao.findCustomerByEmail("asdcdss@dsb"); // output will be null
+//			System.out.println(customer);
+//		} catch (ClassNotFoundException | SQLException e)
+//		{
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+    	
+    	
     }
 }
---
+++ src/test/java/com/ecommerce/CustomerDAOTest.java
@@ -0,0 +1,6 @@
+package com.ecommerce;
+
+public class CustomerDAOTest
+{
+	
+}
